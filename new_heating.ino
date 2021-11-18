69c68b843201c55e476ccd27ddfd56fdce741a62
8none1 <will@whizzy.org>
2021-11-18 13:44:58 +0000

    - Refactor  - Add room stat

--- new_heating.ino
+++ new_heating.ino
@@ -13,27 +13,28 @@
 #include "wificreds.h"
 
 #define TEMPERATURE_PRECISION 9
-#define MAX_HW_TEMP 5600
 
 // Define your wifi creds in wificreds.h
 
 
-const char* ssid = STASSID;
-const char* password = STAPSK;
+// Consts
+const char* SSID         = STASSID;
+const char* WIFIPASS     = STAPSK;
+const byte  PWR_RELAY    = 16;
+const byte  CH_RELAY     = 14;
+const byte  HW_RELAY     = 12;
+const byte  THERMO_RELAY = 13;
+const byte  PWR_LED      = 15;
+const byte  CH_LED       = 2;
+const byte  HW_LED       = 4;
+const byte  PWR_SW       = 0;
+const byte  CH_SW      = 1; // TX_GPIO
+const byte  HW_SW      = 3; // RX_GPIO
+const unsigned int MAX_RUN_TIME = 5400;
 
-const byte PWR_RELAY = 16;
-const byte CH_RELAY = 14;
-const byte HW_RELAY = 12;
-const byte THERMO_RELAY = 13;
-const byte PWR_LED = 15;
-const byte CH_LED = 2;
-const byte HW_LED = 4;
-const byte PWR_SW = 0;
-bool enabled = false;
-bool ch_on = false;
-bool hw_on = false;
-const unsigned int max_run_time = 5800;
-
+// Globals
+bool ENABLED = false;
+int  MAX_HW_TEMP = 5600;
 
 // Server
 ESP8266WebServer server(80);
@@ -73,17 +74,17 @@ File f;
 
 void logger(String message){
   unsigned int fsize = f.size();
-  if (fsize > 5000) {
+  if (fsize > 100000) {
     f.close();
     SPIFFS.gc();
     SPIFFS.remove("/log.txt");
     f = SPIFFS.open("/log.txt", "a+");
+    f.println("Log cleared");
   }
   f.seek(0,SeekEnd);
   f.print(timeClient.getFormattedTime());
   f.print(" : ");
   f.println(message);
-  Serial.println(message);
 }
 
 void store_temperature(String device_name, int16_t temperature) {
@@ -110,16 +111,12 @@ int16_t onewire_reading(){
   for (int i=0; i < temperature_device_count; i++){
     if(sensors.getAddress(tempDeviceAddress, i)) {
       String device_address_str = getDeviceAddressString(tempDeviceAddress);
-      //String device_address_str = "";
-      // for (uint8_t i = 0; i < 8; i++) {
-      //   if (tempDeviceAddress[i] < 16) device_address_str += "0";
-      //   device_address_str += String(tempDeviceAddress[i], HEX);
-      // }
       int16_t raw_temp = sensors.getTemp(tempDeviceAddress);
       raw_temp = raw_temp * 100 / 128; // Convert to c
       if (raw_temp > max_temp) max_temp = raw_temp;
       if (raw_temp == DEVICE_DISCONNECTED_RAW) {
-        logger("Failed to read sensor");
+        String message = "Failed to read one wire sensor: " + device_address_str;
+        logger(message);
         return DEVICE_DISCONNECTED_RAW;
       }
       String json_string = "{\"" + device_address_str + "\":"+String(raw_temp)+"}";
@@ -136,36 +133,22 @@ int16_t onewire_reading(){
 }
 
 void handleLog() {
-  unsigned int s = f.size();
-  Serial.print("Log bytes: ");
-  Serial.println(s);
   if (f) {
     f.seek(0, SeekSet);
     size_t sent = server.streamFile(f, "text/plain");
-  } else {
-    Serial.println("Failed to send log");
   }
-}
-
-void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
 }
 
 void handleRoot() {
   server.send_P(200, "text/html", html_blob);
 }
 
+void handleSetMaxTemp(String new_max_temp){
+  String message = String(new_max_temp);
+  message = "Setting new MAX_HW_TEMP: " + message;
+  logger(message);
+  MAX_HW_TEMP = new_max_temp.toInt();
+}
 
 void controllerStatus(String actor) {
   // GET current status and return a JSON object
@@ -191,6 +174,9 @@ void controllerStatus(String actor) {
     String btm = String(temperature_btm);
     hwc = ", \"top\":" + top + ", \"mid\":" + mid + ", \"btm\":" + btm;
   }
+  else if (actor == "roomstat") {
+    status = digitalRead(THERMO_RELAY);
+  }
   else {
     status = false;
   }
@@ -205,102 +191,120 @@ void controllerStatus(String actor) {
   server.send(200, "application/json", message);
 }
 
-
-void controllerAction(String actor, String action, String dur = String(max_run_time)){
+void controllerAction(String actor, String action, String dur = String(MAX_RUN_TIME)){
   unsigned int duration = dur.toInt();
   // We expect duration in minutes from the old system.  Need to convert to seconds
-  if (duration != max_run_time) duration = duration * 60;
-  if (duration > max_run_time) duration = max_run_time;
-  String offtime = "";
-  String state   = "";
+  if (duration != MAX_RUN_TIME) duration = duration * 60;
+  if (duration > MAX_RUN_TIME) duration = MAX_RUN_TIME;
+  unsigned long off_epoch = current_epoch + duration;
 
-  if ((actor == "psu") && (action == "on") && (enabled == false)) {
-    logger("Enabling PSU and Controller");
-    digitalWrite(PWR_RELAY, HIGH);
-    digitalWrite(PWR_LED, LOW);
-    enabled = true;
-    state = "true";
+  String offtime;
+  String message;
+  bool s = (action == "on") ? 1 : 0;
+  String state = (s) ? "true" : "false";
+
+  if (actor=="psu"){
+    digitalWrite(PWR_RELAY, s);
+    digitalWrite(PWR_LED, !s);
+    ENABLED = s;
+    if (!s) {
+      ch_off_epoch = 0;
+      hw_off_epoch = 0;
+      digitalWrite(HW_RELAY, LOW);
+      digitalWrite(CH_RELAY, LOW);
+      digitalWrite(HW_LED, HIGH);
+      digitalWrite(CH_LED, HIGH);
+    }
+    logger("Changing PSU state:" + state);
   }
-  else if ((actor == "psu") && (action == "off")) {
-    logger("Disabling PSU and Controller");
-    digitalWrite(PWR_RELAY, LOW);
-    digitalWrite(HW_RELAY, LOW);
-    digitalWrite(CH_RELAY, LOW);
-    ch_off_epoch = 0;
-    hw_off_epoch = 0;
-    enabled = false;
-    state = "false";
+
+  else if ((actor == "hw") && (ENABLED)){
+    digitalWrite(HW_RELAY, s);
+    digitalWrite(HW_LED, !s);
+    if (s) {
+      hw_off_epoch = off_epoch;
+      offtime = ",\"offtime\":" + String(hw_off_epoch);
+    }
+    else {
+      hw_off_epoch = 0;
+    }
+    logger("Changing HW state: " + action);
   }
-  else if ((actor == "hw") && (action == "on") && (enabled == true)) {
-    logger("Turn on hot water relay");
-    hw_off_epoch = current_epoch + duration;
-    digitalWrite(HW_RELAY, HIGH);
-    offtime = ",\"offtime\":" + String(hw_off_epoch);
-    state = "true";
+
+  else if ((actor == "ch") && (ENABLED)) {
+    digitalWrite(CH_RELAY, s);
+    digitalWrite(CH_LED, !s);
+    if (s) {
+      ch_off_epoch = off_epoch;
+      offtime = ",\"offtime\":" + String(ch_off_epoch);
+    }
+    else ch_off_epoch = 0;
+    logger("Changing CH state: " + action);
   }
-  else if ((actor == "hw") && (action == "off")) {
-    digitalWrite(HW_RELAY, LOW);
-    hw_off_epoch = 0;
-    state = "false";
-    logger("Switching HW Relay Off");
+
+
+  else if (actor == "roomstat") {
+    digitalWrite(THERMO_RELAY, s);
+    logger("Changed room state: "+ action);
   }
-  else if ((actor == "ch") && (action == "on") && (enabled == true)) {
-    logger("Turn on heating relay");
-    ch_off_epoch = current_epoch + duration;
-    digitalWrite(CH_RELAY, HIGH);
-    offtime = ",\"offtime\":" + String(ch_off_epoch);
-    state = "true";
-  }
-  else if ((actor == "ch") && (action == "off")) {
-    digitalWrite(CH_RELAY, LOW);
-    ch_off_epoch = 0;
-    state = "false";
-    logger("Switching CH relay off");
-  }
+
   else {
-    if (!enabled) server.send(404, "application/json", "{\"state\":false,\"message\":\"Controller not enabled\",\"result\":true}");
+    if (!ENABLED) server.send(404, "application/json", "{\"state\":false,\"message\":\"Controller not enabled\",\"result\":true}");
     else server.send(404, "application/json", "{\"status\":false,\"message\":\"Failed to parse POSTed options\",\"result\":false}");
     return;
   }
-  String message = "{\"result\":true, \"state\":" + state + offtime + "}";
+  message = "{\"result\":true, \"state\":" + state + offtime + "}";
   server.send(200, "application/json", message);
 }
 
+void handleNotFound() {
+  String message = "File Not Found\n";
+  server.send(404, "text/plain", message);
+}
+
 void setup() {
-  Serial.begin(115200);
+  //Serial.begin(115200);
   // Open the log file early
   if (SPIFFS.begin()) {
     f = SPIFFS.open("/log.txt", "a+");
-    if (!f) Serial.println("Failed to open log file");
+    //if (!f) Serial.println("Failed to open log file");
   } else {
+    pinMode(CH_SW, FUNCTION_0);
+    pinMode(HW_SW, FUNCTION_0);
     Serial.begin(115200);
-    Serial.println("Failed to open SPIFFS");
+    Serial.println("Failed to open SPIFFS for log file.  Can't continue.");
+    delay(5000);
     ESP.restart();
   }
-  logger("Booting");
+  logger("\n\n\n\n----------\nBooting");
   
   WiFi.mode(WIFI_STA);
   WiFi.hostname("new_heat");
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, WIFIPASS);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    logger("Connection Failed! Rebooting...");
+    logger("Couldn't connect to Wifi! Rebooting...");
     delay(5000);
     ESP.restart();
   }
 
   // Set up ins and outs
   pinMode(PWR_RELAY, OUTPUT);
-  digitalWrite(PWR_RELAY, LOW);
   pinMode(CH_RELAY, OUTPUT);
   pinMode(HW_RELAY, OUTPUT);
   pinMode(THERMO_RELAY, OUTPUT);
+  digitalWrite(PWR_RELAY, LOW);
+  digitalWrite(CH_RELAY, LOW);
+  digitalWrite(HW_RELAY, LOW);
+  digitalWrite(THERMO_RELAY, LOW);
   pinMode(PWR_LED, OUTPUT);
   pinMode(CH_LED, OUTPUT);
   pinMode(HW_LED, OUTPUT);
   pinMode(PWR_SW, INPUT_PULLUP);
-  digitalWrite(CH_RELAY, LOW);
-  digitalWrite(HW_RELAY, LOW);
-  digitalWrite(THERMO_RELAY, LOW);
+  pinMode(CH_SW, FUNCTION_3);
+  pinMode(HW_SW, FUNCTION_3);
+  pinMode(CH_SW, INPUT_PULLUP);
+  pinMode(HW_SW, INPUT_PULLUP);
+
   
   // Set up OTA updates
   ArduinoOTA.setHostname("new_heat_ota");
@@ -314,52 +318,42 @@ void setup() {
     f.close();
     SPIFFS.gc();
     SPIFFS.end();
-    logger("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    logger("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    logger(String(progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    logger("OTA Error");
-    if (error == OTA_AUTH_ERROR) {
-      logger("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      logger("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      logger("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      logger("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      logger("End Failed");
-    }
   });
   ArduinoOTA.begin();
 
   // NTP
   timeClient.begin();
   timeClient.update();
+  logger("Connected to NTP server");
+  
 
   // MQTT
-  logger("Attempting to connect to the MQTT broker: ");
-  if (!mqttClient.connect(broker, port)) {
-    logger("MQTT connection failed!");
-  }
+  logger("Connecting to the MQTT broker");
+  if (!mqttClient.connect(broker, port)) logger("MQTT connection failed!");
 
   // OneWire
   sensors.begin();
   temperature_device_count = sensors.getDeviceCount();
+  String message = "Found 1wire temperature devices: ";
+  message += String(temperature_device_count);
+  logger(message);
   DeviceAddress tempDeviceAddress;
   for(int i=0;i<temperature_device_count; i++)  {
-    // Search the wire for address
     if(sensors.getAddress(tempDeviceAddress, i)) {
       sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
     }
   }
     
+  // Set up HTTP server responders
   server.on("/", HTTP_GET, handleRoot);
+  server.on("/log", HTTP_GET, handleLog);
+  server.on("/get/max_hw_temp", HTTP_GET, []() {
+    String message = "{\"max_hw_temp\":";
+    message += String(MAX_HW_TEMP);
+    message += "}";
+    logger(message);
+    server.send(200, "application/json", message);
+  });
   server.on(UriBraces("/get/{}"), HTTP_GET, []() {
     String actor = server.pathArg(0);
     controllerStatus(actor);
@@ -375,24 +369,25 @@ void setup() {
     String duration = server.pathArg(2);
     controllerAction(actor, action, duration);
   });
-  server.on("/log", HTTP_GET, handleLog);
+  server.on(UriBraces("/set/max_hw_temp/{}"), HTTP_POST, []() {
+    String new_max_temp = server.pathArg(0);
+    handleSetMaxTemp(new_max_temp);
+  });
   server.onNotFound(handleNotFound);
+  logger("Starting web server");
   server.begin();
   
   if (MDNS.begin("new-heat")) {
     logger("MDNS responder started");
   }
 
-  logger("Ready");  
+  logger("Setup complete. Ready.");  
 }
-
-
 
 void loop() {
   ArduinoOTA.handle();
   server.handleClient();
   mqttClient.poll();
-  timeClient.update();
   
   // Heat and hot water logic:
   // # Function  |  HW_OFF |  HW_ON  |  CH_ON
@@ -412,7 +407,6 @@ void loop() {
       logger("HW time up.  Turning off");
       digitalWrite(HW_RELAY, LOW);
       hw_off_epoch = 0;
-
   }
 
   // Status LEDs are set by looking at the relays.
@@ -422,19 +416,48 @@ void loop() {
   
 
   // Switch stuff
+  ///  PWR Switch
   if (digitalRead(PWR_SW) == LOW) {
-    logger("Button pressed");
+    logger("PWR Button pressed");
     unsigned long st = millis();
     while (!digitalRead(PWR_SW) && (millis()-st < 2000)) {
-      //logger("Button held");
       delay(10);
     };
-    logger("Button released");
+    logger("PWR Button released");
     if (millis() - st > 2000) {
-      enabled = !enabled;
-      digitalWrite(PWR_LED, !enabled); // Logic is reversed for LEDs since they are pull down
-      digitalWrite(PWR_RELAY, enabled); // Relays are normal logic
-      delay(2000);  // Give the user time to let go of the button when they here the click
+      String s = (!ENABLED) ? "on" : "off";
+      controllerAction("psu", s);
+      }
+      delay(1000);  // Give the user time to let go of the button when they here the click
+  };
+
+  /// HW Switch
+  if (digitalRead(HW_SW) == LOW) {
+    logger("HW Button pressed");
+    unsigned int st = millis();
+    while (!digitalRead(HW_SW) && (millis()-st < 1000)) {
+      delay(10);
+    };
+    logger("HW button released or timer exceeded");
+    if ((millis() - st >= 1000) && (ENABLED)) {
+      String s = (!digitalRead(HW_RELAY)) ? "on" : "off";
+      controllerAction("hw", s);
+      delay(1000);
+    }
+  };
+
+  /// CH Switch
+  if (digitalRead(CH_SW) == LOW) {
+    logger("CH Button pressed");
+    unsigned int st = millis();
+    while (!digitalRead(CH_SW) && (millis()-st < 1000)) {
+      delay(10);
+    };
+    logger("CH button released or timer exceeded");
+    if ((millis() - st > 1000) && (ENABLED)) {
+      String s = (!digitalRead(CH_RELAY)) ? "on" : "off";
+      controllerAction("ch", s);
+      delay(1000);
     }
   };
 
@@ -442,7 +465,7 @@ void loop() {
   if (millis() - millis_1s > 1000) {
     millis_1s = millis();
     current_epoch = timeClient.getEpochTime();
-    if (!enabled) {
+    if (!ENABLED) {
       // We are off, so flash the PWR light to show standby mode.
       digitalWrite(PWR_LED, !digitalRead(PWR_LED));
     }
@@ -450,15 +473,13 @@ void loop() {
 
   // 10 minute pulse
   if (millis() - millis_10m > 600000) {
-    logger("10m tick");
+    timeClient.update();
     millis_10m = millis();
     int16_t max_temp = onewire_reading();
     if (max_temp > MAX_HW_TEMP) {
-      // Hot water tank should be full now, turn off HW
+      logger("HW is up to temperature.  Turning off.");
       digitalWrite(HW_RELAY, LOW);
       hw_off_epoch = 0;
     }
-    String t = String(timeClient.getFormattedTime());
-    logger(t);
   }
 } 
